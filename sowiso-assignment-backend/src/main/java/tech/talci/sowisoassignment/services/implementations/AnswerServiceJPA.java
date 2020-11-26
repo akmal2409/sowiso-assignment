f9d62bd7d535b47d9cb3b9157cb4a4afc2fcd468
akmal2409 <akmalalikhujaev@gmail.com>
2020-11-26 10:49:18 +0100

    Implemented AnswerServiceJpa

--- sowiso-assignment-backend/src/main/java/tech/talci/sowisoassignment/services/implementations/AnswerServiceJPA.java
+++ sowiso-assignment-backend/src/main/java/tech/talci/sowisoassignment/services/implementations/AnswerServiceJPA.java
@@ -2,12 +2,42 @@ package tech.talci.sowisoassignment.services.implementations;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import tech.talci.sowisoassignment.domain.Answer;
+import tech.talci.sowisoassignment.exceptions.ResourceNotFoundException;
 import tech.talci.sowisoassignment.repositories.AnswerRepository;
 import tech.talci.sowisoassignment.services.AnswerService;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class AnswerServiceJPA implements AnswerService {
 
     private final AnswerRepository answerRepository;
+
+    @Override
+    public List<Answer> findAll() {
+        return answerRepository.findAll();
+    }
+
+    @Override
+    public Answer findById(Long id) {
+        return answerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Answer was not found"));
+    }
+
+    @Override
+    public Answer save(Answer answer) {
+        return answerRepository.save(answer);
+    }
+
+    @Override
+    public void delete(Answer answer) {
+        answerRepository.delete(answer);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        answerRepository.deleteById(id);
+    }
 }
